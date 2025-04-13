b561a8b3880d72506b1b4f2034ddbde48ec755cb
ArdianDragusha <ardian.dragusha@myy.haaga-helia.fi>
2025-04-13 16:01:44 +0300

    testit

--- src/test/java/hh/backend/matchlist/MatchlistApplicationTests.java
+++ src/test/java/hh/backend/matchlist/MatchlistApplicationTests.java
@@ -1,13 +1,33 @@
 package hh.backend.matchlist;
 
+import hh.backend.matchlist.domain.Match;
+import hh.backend.matchlist.domain.MatchRepository;
+import hh.backend.matchlist.domain.Competition;
+import hh.backend.matchlist.domain.CompetitionRepository;
+import hh.backend.matchlist.domain.User;
+import hh.backend.matchlist.domain.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MatchlistApplicationTests {
 
+	@Autowired
+	private MatchRepository matchRepository;
+
+	@Autowired
+	private CompetitionRepository competitionRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+		assertThat(matchRepository).isNotNull();
+		assertThat(competitionRepository).isNotNull();
+		assertThat(userRepository).isNotNull();
 	}
-
 }
